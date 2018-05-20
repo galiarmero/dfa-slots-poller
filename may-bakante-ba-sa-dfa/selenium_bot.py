31f9d5e094cbf51055e5aee7038d77162280bda6
Gali Armero <galiarmero.io@gmail.com>
2018-05-21 00:21:45 +0800

    Format results

--- may-bakante-ba-sa-dfa/selenium_bot.py
+++ may-bakante-ba-sa-dfa/selenium_bot.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import requests
+import datetime
 
 
 APAC_REGION_ID = 1 # Asia Pacific
@@ -30,9 +31,13 @@ class SeleniumBot(object):
                 site_id = site['Id']
                 next_available_timeslot = self._get_next_available_timeslot( \
                     '2018-05-16', '2019-05-16', site_id)
-                timeslots_availability = self._get_timeslots_availability( \
+                available_timeslots = self._get_timeslots_availability( \
                     '2018-05-16', '2019-05-16', site_id)
-                print(site['Name'], ':', next_available_timeslot)
+
+                print(site['Name'])
+                print(' > Next Available  : {}'.format(next_available_timeslot))
+                print(' > Available {0:5s} : {1}'.format( \
+                    '({})'.format(len(available_timeslots)), available_timeslots))
 
         except Exception as ex:
             print("{}: {}".format(type(ex).__name__, ex))
@@ -53,16 +58,23 @@ class SeleniumBot(object):
 
 
     def _get_next_available_timeslot(self, from_date, to_date, site_id):
-        return self._session.post(NEXT_AVAILABLE_TIMESLOT_URI, \
+        next_available = self._session.post(NEXT_AVAILABLE_TIMESLOT_URI, \
                 data={'requestDate': from_date, 'maxDate': to_date, 'siteId': site_id, 'slots': 1}, \
                 headers=SCHEDULE_XHR_HEADERS).json()
 
+        return self._millis_to_date(next_available['Date']) if 'Date' in next_available else None
+
 
     def _get_timeslots_availability(self, from_date, to_date, site_id):
-        return self._session.post(AVAILABLE_TIMESLOT_URI, \
+        timeslots = self._session.post(AVAILABLE_TIMESLOT_URI, \
                 data={'fromDate': from_date, 'toDate': to_date, 'siteId': site_id, 'requestedSlots': 1}, \
                 headers=SCHEDULE_XHR_HEADERS).json()
 
+        return [ self._millis_to_date(t['AppointmentDate']) for t in timeslots if t['IsAvailable'] ]
+
+    def _millis_to_date(self, millis):
+        return datetime.datetime.fromtimestamp(millis / 1000.0) \
+                                .strftime('%a, %b %d %Y %I:%M %p')
 
 if __name__ == "__main__":
     bot = SeleniumBot()
